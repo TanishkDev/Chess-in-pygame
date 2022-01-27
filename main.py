2c07832d2f77ca10bb381be9f5bee726a7145949
TanishkDev <anynmoussirohi@gmail.com>
2022-01-27 19:50:03 +0530

    all pieces set

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 from setting import widht, height, chess_board_x, chess_board_y
 from board import Board
-from pieces import Pawn
+from pieces import *
 
 pygame.init()
 
@@ -14,30 +14,116 @@ class Game:
         self.board = Board(screen)
         self.setup_board()
 
-    def add_group_pieces(self, pieces,no_of_pieces):
-        sprite_group = pygame.sprite.GroupSingle()
+    def add_group_pieces(self, pieces, no_of_pieces=None):
+        sprite_group = pygame.sprite.Group()
         if pieces == "white_pawn":
             for i in range(no_of_pieces):
-                pos = [128+64*i,64+64*1]
+                pos = [128+64*i, 64+64*1]
                 sprite = Pawn("w", pos)
                 sprite_group.add(sprite)
-            
+
         if pieces == "black_pawn":
             for i in range(no_of_pieces):
-                pos = [128+64*i,64+64*6]
+                pos = [128+64*i, 64+64*6]
                 sprite = Pawn("b", pos)
                 sprite_group.add(sprite)
-            
+
+        if pieces == "white_knight":
+            for i in range(no_of_pieces):
+                if i == 0:
+                    pos = [128+64*1, 64+64*0]
+                if i == 1:
+                    pos = [128+64*6, 64+64*0]
+                sprite = Knight("w", pos)
+                sprite_group.add(sprite)
+
+        if pieces == "black_knight":
+            for i in range(no_of_pieces):
+                if i == 0:
+                    pos = [128+64*1, 64+64*7]
+                if i == 1:
+                    pos = [128+64*6, 64+64*7]
+                sprite = Knight("b", pos)
+                sprite_group.add(sprite)
+
+        if pieces == "white_rook":
+            for i in range(no_of_pieces):
+                if i == 0:
+                    pos = [128+64*0, 64+64*0]
+                if i == 1:
+                    pos = [128+64*7, 64+64*0]
+                sprite = Rook("w", pos)
+                sprite_group.add(sprite)
+
+        if pieces == "black_rook":
+            for i in range(no_of_pieces):
+                if i == 0:
+                    pos = [128+64*0, 64+64*7]
+                if i == 1:
+                    pos = [128+64*7, 64+64*7]
+                sprite = Rook("b", pos)
+                sprite_group.add(sprite)
+
+        if pieces == "white_bishop":
+            for i in range(no_of_pieces):
+                if i == 0:
+                    pos = [128+64*2, 64+64*0]
+                if i == 1:
+                    pos = [128+64*5, 64+64*0]
+                sprite = Bishop("w", pos)
+                sprite_group.add(sprite)
+
+        if pieces == "black_bishop":
+            for i in range(no_of_pieces):
+                if i == 0:
+                    pos = [128+64*2, 64+64*7]
+                if i == 1:
+                    pos = [128+64*5, 64+64*7]
+                sprite = Bishop("b", pos)
+                sprite_group.add(sprite)
+
+        if pieces == "white_queen":
+            pos = [128+64*3, 64+64*0]
+            sprite = Queen("w", pos)
+            sprite_group.add(sprite)
+
+        if pieces == "black_queen":
+            pos = [128+64*3, 64+64*7]
+            sprite = Queen("b", pos)
+            sprite_group.add(sprite)
+
+        if pieces == "white_king":
+            pos = [128+64*4, 64+64*0]
+            sprite = King("w", pos)
+            sprite_group.add(sprite)
+
+        if pieces == "black_king":
+            pos = [128+64*4, 64+64*7]
+            sprite = King("b", pos)
+            sprite_group.add(sprite)
+
         return sprite_group
 
-    def setup_board(self):  
-        self.white_pieces = {0: self.add_group_pieces("white_pawn",8)}
-        print(self.white_pieces[0].sprite.rect)
-        self.black_pieces = {0: self.add_group_pieces("black_pawn",8)}
+    def setup_board(self):
+        self.white_pieces = {0: self.add_group_pieces("white_pawn", 8),
+                             1: self.add_group_pieces("white_knight", 2),
+                             2: self.add_group_pieces("white_bishop", 2),
+                             3: self.add_group_pieces("white_rook", 2),
+                             4: self.add_group_pieces("white_queen"),
+                             5: self.add_group_pieces("white_king")}
+
+        self.black_pieces = {0: self.add_group_pieces("black_pawn", 8),
+                             1: self.add_group_pieces("black_knight", 2),
+                             2: self.add_group_pieces("black_bishop", 2),
+                             3: self.add_group_pieces("black_rook", 2),
+                             4: self.add_group_pieces("black_queen"),
+                             5: self.add_group_pieces("black_king")}
 
     def draw(self):
-        self.white_pieces[0].draw(screen)
-        self.black_pieces[0].draw(screen)
+        # use dict comprehension once all sprite are completed
+        for i in range(6):
+            self.white_pieces[i].draw(screen)
+            self.black_pieces[i].draw(screen)
 
     def run(self):
         self.board.draw()
